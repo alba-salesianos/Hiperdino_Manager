fc5eca47b1c0806cc691aa4ed88f4a84468c1572
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-05-02 18:54:55 +0100

    feat: added customer class and methods to manipulate shopping cart

--- src/es/hiperdino/Main.java
+++ src/es/hiperdino/Main.java
@@ -1,10 +1,21 @@
 package es.hiperdino;
 
+import es.hiperdino.entities.Customer;
 import es.hiperdino.utils.RandomGenerator;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(RandomGenerator.getRandomProduct());
         System.out.println(RandomGenerator.getRandomName());
+
+
+        Customer cliente1 = new Customer();
+
+        cliente1.addItemToCart();
+        cliente1.addItemToCart();
+        cliente1.addItemToCart();
+        System.out.println(cliente1);
+
+
     }
 }
---
+++ src/es/hiperdino/entities/Customer.java
@@ -0,0 +1,37 @@
+package es.hiperdino.entities;
+
+import es.hiperdino.utils.RandomGenerator;
+
+import java.util.Stack;
+
+public class Customer {
+    private String name;
+    private int numberOfItems;
+    private Stack<String> shoppingCart = new Stack<>();
+
+    public Customer() {
+        this.name = RandomGenerator.getRandomName();
+        this.numberOfItems = 0;
+    }
+
+    public void addItemToCart(){
+        shoppingCart.add(RandomGenerator.getRandomProduct());
+        numberOfItems++;
+    }
+
+    public String displayCart(){
+        String message = shoppingCart.toString().replace("[","").replace("]","").replace(",","\n  -");
+        return message = "  - " + message;
+    }
+
+    @Override
+    public String toString() {
+        return "===================================" +
+                "\n* Nombre: " + name +
+                "\n* Total de productos: " + numberOfItems +
+                "\n* Lista de artículos en la cesta:\n" +
+                displayCart() +
+                "\n ===================================";
+    }
+}
+
